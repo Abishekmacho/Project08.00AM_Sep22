b4f53135f020d716690e399b00df55e92c5ed42c
Abishekmacho <abishekmacho@gmail.com>
2022-09-07 18:27:22 +0530

    Resolved Conflicts

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -1,20 +1,16 @@
 package org.test;
 
 public class Sample {
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		System.out.println("Test3");
-		System.out.println("Test4");
-		System.out.println("Test5");
-	}
-=======
-	
-	public static void main(String[] args) {
-		System.out.println("Test1");
-		System.out.println("Test2");
-		System.out.println("Test3");
-	}
-	
->>>>>>> 0a9d3d54cf67edd78a27b5716969a95daa44efdb
 
+	public static void main(String[] args) {
+	
+	System.out.println("Test1");
+	System.out.println("Test2");
+	System.out.println("Test3");
+    System.out.println("Test3");
+    System.out.println("Test4");
+    System.out.println("Test5");
+	
+}
+	
 }
